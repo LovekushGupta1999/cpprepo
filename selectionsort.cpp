7dbd368da14a04705eaad78edd4e5a5a85f3f65b
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-12-24 18:24:43 +0530

    program

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int selection_sort(int a[], n){
+int selection_sort(int a[], int n){
     for(int i=0; i<n; ++i){
         for(int j=i+1; i<n; ++i){
             if(a[i]>a[j]){
@@ -12,12 +12,18 @@ int selection_sort(int a[], n){
             }
         }
     }
+     for(int i=0; i<n; ++i){
+                cout<<a[i];}
 }
 int main() {
    int a[]={1,5,4,8,7,6};
    int size=sizeof a/sizeof a[0];
-   int b[]=selection_sort(a,size);
-            for(int i=0; i<size; ++i){
-                cout<<a[i];}
+   for(int i=0; i<size; ++i){
+                cout<<a[i];
+    }
+   cout<<"after sorting";
+
+   selection_sort(a,size);
+            
     return 0;
 }
