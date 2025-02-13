d91b0e2d95663b0d0b1fa98f41baabc5ab430b29
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-02-13 22:05:48 +0530

    print unique subset of array

---
+++ printuniquesubsetofgivenduplicateelementofarray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void subset(int arr[], vector<int>& b, int n, int length) {
+    if (n == length) {
+        cout << "{";
+        for (int i = 0; i < b.size(); ++i) {
+            cout << b[i];
+            if (i < b.size() - 1) cout << ", ";
+        }
+        cout << "}\n";
+        return;
+    }
+
+    b.push_back(arr[n]);
+    subset(arr, b, n + 1, length);
+    
+    b.pop_back();
+    for(int i=n; i<length-1; i++){
+        if(arr[n]==arr[n+1]){
+            n++;
+        }
+    }
+    subset(arr, b, n + 1, length);
+}
+
+int main() {
+    int arr[] = {1, 2, 2};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    vector<int> b;
+    
+    subset(arr, b, 0, length);
+    
+    return 0;
+}
